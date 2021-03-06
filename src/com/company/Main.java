624cd03024adec28a845a26471b9f0e1afaeade7
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2021-03-06 12:06:31 -0500

    Adding in random number generator, and start of loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,23 +23,43 @@ public class Main {
         // Here is the solution with the steps:
 
         // 1. Let's create some variables to hold relevant information
-        int userGuess = 0; // to hold the users guess, 0 to start
+        int userGuess = 0; // to hold the users guess
         int correctAnswer = 0; // to hold the correct answer - we will generate in a minute
         int range = 10; // to hold the maximum value the random number can be
         int numberOfGuesses = 0; // to hold the number of guesses
 
-        // 2. Let's get the prompts we need out of the way
+        // 2. Let's get the prompts we need out of the way. This is just to make the code cleaner in the actual game below.
         String welcomeMessage = "------------------Number Guessing Game---------------------\n\n" +
                                 "Welcome to the number guessing game!\n";
         String rangeMessage = "Please enter the maximum number to guess to:";
+        String letsGoPrompt = "Great! Let's begin.\n";
+        String entryPrompt = "Please enter your guess:";
         String tooHighPrompt = "Your guess was too high. Please try again.\n";
         String tooLowPrompt = "Your guess was too low. Please try again.\n";
         String correctPrompt = "You got it! Great work.\n";
 
-        // 3. Lets start the program! First, we need to create an infinite loop. We will use breaks and continues
-        //    to exit or continue on in the loop
+        // Let's also create error messages too. We will use these when we get to exception handling.
+        String invalidEntry = "Invalid entry. Please enter a positive whole number.";
 
-        while(true){
+        // 3. Lets create a Scanner object so we can take in user input from the command line
+        Scanner scanner = new Scanner(System.in); // This is a new Scanner object with the name of "scanner".
+
+        // 4. Let's print out the start of the program, and ask the user for a range
+        System.out.println(welcomeMessage);
+        System.out.println(rangeMessage);
+        range = scanner.nextInt(); // grab entry and store in the "range" variable.
+
+        // 5. Let's generate a random number by doing the following:
+        //    a. using Math.random() to generate a double between 0.0 and 1.0
+        //    b. multiplying that decimal by the users range value to get our number, but currently as decimal
+        //    c. convert it to an integer and floor it to make it a whole number
+        correctAnswer = (int)(Math.random() * range);
+
+        System.out.println(letsGoPrompt);
+
+        // 5. Now we start the infinite loop of the game.
+        while(true){ // this loop will run forever until we "break" out of it
+            System.out.println(entryPrompt);
 
 
 
